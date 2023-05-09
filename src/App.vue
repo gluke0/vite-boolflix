aa6228b9cb5acf5db63daf3420cdc748223a9c37
gluke0 <milanesigianluca@gmail.com>
2023-05-09 15:51:18 +0200

    ricerca per titolo, tit orig, ling e stelle

--- src/App.vue
+++ src/App.vue
@@ -9,25 +9,44 @@ export default{
             store
         }       
     },
-    
-    created(){
-        axios.get(store.alldata.movieApi,{
-            params: {
-                apiKey: store.alldata.apiKey,
-                query: store.alldata.query
-            }
-        })
-        .then ((res) => {
-            console.log(res.data);
-        })
-    }
+    methods: {
+        search(){
+            axios.get(store.alldata.movieApi,{
+                params: {
+                    api_key: store.alldata.apiKey,
+                    query: store.query
+                }
+            })
+            .then ((res) => {
+                store.searched = res.data.results;
+            })
+        }
+    }  
 }
 
 </script>
 
 
 <template>
-  
+    
+    <div class="bg-primary">
+        <div class="container m-5">
+            <form @submit.prevent="search">
+                <input v-model="store.query" type="search" name="search-movie" id="search-movie">
+                <button> Search </button>
+            </form>
+        </div>
+
+        <div class="container m-5">
+            <ul v-for="found in store.searched">
+                <li><strong>titolo:</strong> {{ found.title }}</li>
+                <li><strong>titolo originale:</strong> {{ found.original_title }}</li>
+                <li><strong>lingua:</strong> {{ found.original_language }}</li>
+                <li><strong>stelle:</strong> {{ found.vote_average }}</li>
+            </ul>
+        </div>
+    </div>
+
 </template>
 
 
--- src/store.js
+++ src/store.js
@@ -4,6 +4,7 @@ export const store = reactive({
     alldata: {
         movieApi: 'http://api.themoviedb.org/3/search/movie',
         apiKey: '0c33757391e4c269e5804ffb1423f5d5',
-        query: 'last'
-    }
+    },
+    query: '',
+    searched: '',
 })
